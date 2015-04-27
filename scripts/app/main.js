a24c9870e9b3f58c4b9c999e64e1080d51ab2de6
Jorge Lainfiesta <jorge.lainfiesta@gmail.com>
2015-04-26 22:26:04 -0600

    Added UI buttons and basic event handler.

--- scripts/app/main.js
+++ scripts/app/main.js
@@ -1,5 +1,10 @@
-define(['./animation', './renderer'], function (animation, renderer) {
-	
+define(['jquery', './animation', './renderer'], function ($, animation, renderer) {
+
+  //Handle buttons for crane
+  $("nav.contextual .crane").on("click", "button", function(){
+    alert($(this).data('action'));
+  });
+  
   //Run animate
   animation.animate();
   
